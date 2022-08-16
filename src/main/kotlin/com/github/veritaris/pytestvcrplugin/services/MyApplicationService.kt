c76401b4eba93816df170ce2da0d082429acda50
GitHub Action <action@github.com>
2022-08-16 11:30:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/veritaris/pytestvcrplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/veritaris/pytestvcrplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.veritaris.pytestvcrplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.veritaris.pytestvcrplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/veritaris/pytestvcrplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/veritaris/pytestvcrplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.veritaris.pytestvcrplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.veritaris.pytestvcrplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
